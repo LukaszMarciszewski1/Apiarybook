27cef41db37f14cd7860a24295fb5107b0ccb1d1
Łukasz Marciszewski <lukasz.marciszewski@appfinity.pl>
2022-06-13 23:10:41 +0200

    fix specialNumber reduce

--- client/src/calculation/apiaryNumbers.ts
+++ client/src/calculation/apiaryNumbers.ts
@@ -1,39 +1,44 @@
-import dayjs from 'dayjs';
-import isToday from 'dayjs/plugin/isToday';
-import { Apiary } from '../models/apiary';
+import dayjs from 'dayjs'
+import isToday from 'dayjs/plugin/isToday'
+import { Apiary } from '../models/apiary'
 dayjs.locale('pl')
 
 dayjs.extend(isToday)
 export const specialNumber = (data: Apiary[] | undefined) => {
-  
-  if(data){
-    const itemsSpecialNumber = data?.map(item => item.specialNumber)
-    const largestNumberOfItems = itemsSpecialNumber?.reduce((a,x) => Math.max(a, (x + 1)))
+  if (data) {
+    const itemsSpecialNumber = data?.map((item) => item.specialNumber)
+    const largestNumberOfItems = itemsSpecialNumber.length
+      ? itemsSpecialNumber?.reduce((a, b) => Math.max(a, b + 1))
+      : 1
     const isToday = dayjs().isToday()
-    console.log(isToday)
-
-    let initialSpNumber = ('00000' + (isToday ? largestNumberOfItems : 1))
-    while(initialSpNumber.charAt(0) === '0' && initialSpNumber.length > 5){
-      initialSpNumber=initialSpNumber.substring(1)
+    let initialSpNumber = '00000' + (isToday ? largestNumberOfItems : 1)
+    while (initialSpNumber.charAt(0) === '0' && initialSpNumber.length > 5) {
+      initialSpNumber = initialSpNumber.substring(1)
     }
     return initialSpNumber
   }
 }
 
 export const apiaryNumber = (data: Apiary[] | undefined) => {
-  if(data) {
+  if (data) {
     const dataObj = {
-      date : dayjs(Date.now()).format('YYYYMMDD'),
-      specialNumber : specialNumber(data)
+      date: dayjs(Date.now()).format('YYYYMMDD'),
+      specialNumber: specialNumber(data),
     }
-  
+
     const stringFromDataObj = Object.values(dataObj).join('')
     const numberFromDataObj = Number(stringFromDataObj)
-    
-    const arrayWithoutZeros = Array.from(stringFromDataObj).map(str => Number(str)).filter(num => num !== 0)
-    const sum = arrayWithoutZeros.reduce((a, b)=> a * b, numberFromDataObj)
-    const numberArray = Array.from(String(sum)).map(str => Number(str))
-    const result = (stringFromDataObj + numberArray[1] + numberArray[6] + (numberArray[numberArray.length - 1]))
+
+    const arrayWithoutZeros = Array.from(stringFromDataObj)
+      .map((str) => Number(str))
+      .filter((num) => num !== 0)
+    const sum = arrayWithoutZeros.reduce((a, b) => a * b, numberFromDataObj)
+    const numberArray = Array.from(String(sum)).map((str) => Number(str))
+    const result =
+      stringFromDataObj +
+      numberArray[1] +
+      numberArray[6] +
+      numberArray[numberArray.length - 1]
     return result
   }
 }

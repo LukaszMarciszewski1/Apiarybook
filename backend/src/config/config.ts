37d8334b3240929247286d81370f60ba0bc013a0
Łukasz Marciszewski <lukasz.marciszewski@appfinity.pl>
2022-06-16 11:36:49 +0200

    final version

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 
 const MONGO_URL = process.env.MONGO_URI || ''
-const SERVER_PORT = Number(process.env.SERVER_PORT)
+const SERVER_PORT = Number(process.env.SERVER_PORT) || 1337
 
 export const config = {
     mongo: {
